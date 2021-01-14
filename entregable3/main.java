ed4c2c4dc4bb7403bf33c6256dc381f43ce52e15
abeja21 <74176323+abeja21@users.noreply.github.com>
2021-01-14 13:13:41 +0100

    commit final :D

--- entregable3/main.java
+++ entregable3/main.java
@@ -7,6 +7,7 @@ static boolean aplicacion=true;
 		Scanner sc=new Scanner(System.in);
 		tareas.revisiontxt();
 		while(aplicacion==true) {
+			System.out.println("");
 	System.out.println("-----TO-DO_APP-----");
 	System.out.println("que desea hacer:");
 	System.out.println("1--añadir tarea.");
@@ -15,7 +16,9 @@ static boolean aplicacion=true;
 	System.out.println("4-- ver tareas completadas.");
 	System.out.println("5-- ver todas las tareas(completas y pendientes).");
 	System.out.println("6--salir.");
+	System.out.println("");
 	int a= sc.nextInt();
+	System.out.println();
 	switch (a) {
 	case 1:tareas.añadir_tarea();
 	break;
@@ -29,11 +32,12 @@ static boolean aplicacion=true;
 		break;
 	case 6:aplicacion=false;
 	System.out.println("Adios :D");
+	tareas.guardar();
 		break;
 		}
 
 	}
-		sc.close();
+		
 	}
 	
 }
--- entregable3/tareas.java
+++ entregable3/tareas.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 public class tareas {
@@ -24,14 +25,7 @@ public static void añadir_tarea() {
 				 tarea tarea1=new tarea(titulo,new Date(),false);
 				 tareas.put(ID, tarea1);
 				ID++;
-				try {
-					FileWriter fichero = new FileWriter("src\\entregable3\\to-do-app.txt",true);
-			        PrintWriter pw = new PrintWriter(fichero);
-			        pw.println(tarea1.toString());
-			        pw.close();
-				} catch (Exception e) {
-					// TODO: handle exception
-				}
+				
 			
 		}
 		
@@ -44,49 +38,70 @@ public static void añadir_tarea() {
 	public static void completar_tarea() {
 		
 		Scanner sc=new Scanner(System.in);
-		ver_tareas();
-		System.out.println("que tarea deseas completar ");
+		ver_pendientes();
+		System.out.println("que tarea deseas completar");
 		int completar=sc.nextInt();
 		tareas.get(completar).SetPendiente(true);
-		sc.close();
+		
 	}
 	public static void ver_pendientes() {
-		for (int i = 1; i <= ID; i++) {
+		for (int i = 1; i < ID; i++) {
 			if(tareas.get(i).GetPendiente()==false) {
-				System.out.println(tareas.get(i));
+				System.out.println(i+"-"+tareas.get(i));
+				System.out.println("");
 			}
 		}
 	}
 	public static void ver_completadas() {
-		for (int i = 0; i < ID; i++) {
+		for (int i = 1; i < ID; i++) {
 			if(tareas.get(i).GetPendiente()==true) {
 				System.out.println(tareas.get(i));
+			}else {
+				System.out.println("no hay tareas completadas");
+				break;
 			}
 	}
 	}
 	public static void revisiontxt() {
 		try {
-			File myObj = new File("src\\entregable3\\to-do-app.txt");
-				Scanner myReader=new Scanner(myObj);
-				String[] elArray= new String[2];
-				int i=0;
-				while(myReader.hasNextLine()) {
-					String data=myReader.nextLine();
-					elArray[i++]=data;
-				}
-				for (int j = 0; j < elArray.length; j++) {
-					String div[ ]=elArray[j].split("_");
-					String titulo=div[0];
-					SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-					Date fechaDate=formatter.parse(div[1]);
-					boolean estado=Boolean.parseBoolean(div[2]);
-					tarea tarea1=new tarea(titulo,fechaDate,estado);
-					 tareas.put(ID, tarea1);
-					ID++;
-				}
-				}catch (Exception e) {
-					System.out.println("tonto");
-					e.printStackTrace();
-				}
+		      File myObj = new File("src\\entregable3\\to-do-app.txt");
+		      Scanner myReader = new Scanner(myObj);
+		      
+		      while (myReader.hasNextLine()) {
+		        String data = myReader.nextLine();
+		        String div[ ]=data.split("_");
+				String titulo=div[0];
+				SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+				Date fechaDate=formatter.parse(div[1]);
+				boolean estado=Boolean.parseBoolean(div[2]);
+				tarea tarea1=new tarea(titulo,fechaDate,estado);
+				 tareas.put(ID, tarea1);
+				ID++;
+		        
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    } catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				
+				
+	}
+	public static void guardar() {
+		try {
+			FileWriter fichero = new FileWriter("src\\entregable3\\to-do-app.txt");
+	        PrintWriter pw = new PrintWriter(fichero);
+	        for (int i = 1; i < ID; i++) {
+	        	pw.println(tareas.get(i));
+		        
+			}
+	        
+	        pw.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 }
