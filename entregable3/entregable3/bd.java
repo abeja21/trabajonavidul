39a7f90a735894ad20b9ab4c05b287b667bee13f
abeja21 <74176323+abeja21@users.noreply.github.com>
2021-03-26 10:07:47 +0100

    conexion total con la base de datos :D

--- entregable3/entregable3/bd.java
+++ entregable3/entregable3/bd.java
@@ -10,13 +10,12 @@ public class bd {
     static final String DB_URL = "jdbc:mariadb://127.0.0.1:3306/dam";
     static final String User = "root";
     static final String Pass = "";
-
-    public static void main(String[] args) {
-        insertar_dato();
-    }
+     static Connection conn=null;
+    static Statement stmt=null;
+    
     public static void crear_tabla(){
-        Connection conn=null;
-        Statement stmt=null;
+
+
         try {
             Class.forName(JDBC_DRIVER);
             System.out.println("connecting to a selected database....");
@@ -26,7 +25,7 @@ public class bd {
             System.out.println("creating table in given database....");
             stmt=conn.createStatement();
             String sql="CREATE TABLE Tarea "
-                    +"(ID INT, "
+                    +"(ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, "
                     +"titulo VARCHAR(255), "
                     +"fecha DATETIME, "
                     +"estado BOOLEAN) ";
@@ -53,45 +52,32 @@ public class bd {
         System.out.println("goodbye");
     }
 
-    public static void insertar_dato(){
-        Connection conn=null;
-        Statement stmt=null;
+    public static Connection conexion_bd(){
+
         try {
-            Class.forName(JDBC_DRIVER);
-            System.out.println("connecting to a selected database....");
-            conn=DriverManager.getConnection(DB_URL,User,Pass);
-            System.out.println("Connected database successfully....");
+            Class.forName(bd.JDBC_DRIVER);
+            conn = DriverManager.getConnection(bd.DB_URL, bd.User, bd.Pass);
 
-            System.out.println("creating table in given database....");
-            stmt=conn.createStatement();
-
-            String sql="INSERT INTO Tarea "
-                    +"VALUES( " +
-                    "1, "+
-                    "'mario', "
-                    +"'2020-10-14 12:00', "
-                    +"1)";
-            stmt.executeUpdate(sql);
-            System.out.println("adding dates into hola...");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }finally {
-            try {
-                if (stmt!=null){
-                    conn.close();
-                }
-            } catch (SQLException se) {
-            }try {
-                if (conn!=null){
-                    conn.close();
-                }
-            }catch (SQLException se) {
-            }
         }
-        System.out.println("goodbye");
+        return conn;
     }
 
+    public static void desconectar_bd(){
+        try {
+            if (stmt!=null){
+                conn.close();
+            }
+        } catch (SQLException se) {
+        }try {
+            if (conn!=null){
+                conn.close();
+            }
+        }catch (SQLException se) {
+        }
+    }
+    }
 
-}
